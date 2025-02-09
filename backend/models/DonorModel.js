5a3225b414f45c0a3527f230fab7d05a52c71554
Sumit Mali <96630029+Sumit-Mali@users.noreply.github.com>
2025-02-09 10:13:27 +0530

    Update DonorModel.js

--- backend/models/DonorModel.js
+++ backend/models/DonorModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 // Donor Schema
 const DonorSchema = new mongoose.Schema({
-	name: String,
-	village: String,
-	amount: String,
-	photo_url: String,
+    name: { type: String, required: true, index: true }, 
+    amount: { type: String, required: true },
+    village: String,
+    photo_url: String
 });
 
 // Donor Model
